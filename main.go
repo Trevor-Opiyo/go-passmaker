518988f0a32bb81569194c7918680972f846c769
Trevor Opiyo <46691585+Trevor-Opiyo@users.noreply.github.com>
2021-01-10 23:40:29 -0600

    initial commit

---
+++ main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"reflect"
+	"strings"
+	"time"
+)
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	options := ""
+	passLen := 9
+	uppers := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	lowers := strings.ToLower(uppers)
+	nums := "1234567890"
+	specs := "!@#$%^&*()_-+=<>?"
+
+	fmt.Println("----------------------------------\n        Password Generator\n----------------------------------")
+	options = addOptions(options, uppers, "upper-case letters")
+	options = addOptions(options, lowers, "lower-case letters")
+	options = addOptions(options, nums, "integers")
+	options = addOptions(options, specs, "special characters")
+	passLen = passLength()
+	generate(passLen, options)
+}
+
+func addOptions(original string, addition string, characterType string) string {
+	var userInput string
+	for {
+		fmt.Println("Include " + characterType + "? [Y/N]")
+		fmt.Scan(&userInput)
+		switch userInput {
+		case "Y", "y", "yes", "Yes", "YES":
+			return original + addition
+		case "N", "n", "no", "No", "NO":
+			return original
+		default:
+			fmt.Println("Please enter a valid response.")
+		}
+	}
+}
+
+func passLength() int {
+	for {
+		var userInput int
+		fmt.Println("Enter an integer length for the password.")
+		fmt.Scan(&userInput)
+		temp := reflect.TypeOf(userInput).Kind()
+		if temp == reflect.Int {
+			return userInput
+		}
+		fmt.Println("Please enter a valid response.")
+	}
+}
+
+func generate(passLen int, options string) {
+outer:
+	for {
+		fmt.Println("The password is:")
+		for iterator := 0; iterator < passLen; iterator++ {
+			opttionsLen := len(options)
+			randIndex := rand.Intn(opttionsLen)
+			fmt.Printf("%c", options[randIndex])
+		}
+		var userInput string
+		fmt.Println("\nWould you like to regenerate another password with the same parameters? [Y/N]")
+		fmt.Scan(&userInput)
+		switch userInput {
+		case "Y", "y", "yes", "Yes", "YES":
+			break
+		case "N", "n", "no", "No", "NO":
+			break outer
+		default:
+			fmt.Println("Please enter a valid response.")
+		}
+	}
+}
